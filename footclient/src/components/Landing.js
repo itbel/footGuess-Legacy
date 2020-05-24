f30ea5f55d115588d2158b5e1a8a67660dd5b9c2
Igor Belem <igortbelem@gmail.com>
2020-05-23 20:47:46 -0400

    minor css changes

--- footclient/src/components/Landing.js
+++ footclient/src/components/Landing.js
@@ -7,7 +7,9 @@ import SideNav from "./SideNav";
 import Results from "./Results";
 const Landing = () => {
   const { state: authState, dispatch } = useContext(AuthContext);
+  const [selectedTour, setSelectedTour] = useState(undefined);
   const [tournaments, setTournaments] = useState([]);
+  const [isLeagueSet, setIsLeagueSet] = useState(false);
   useEffect(() => {
     Axios.post(
       "http://localhost:3001/tournaments/gettournament",
@@ -22,7 +24,7 @@ const Landing = () => {
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }, [isLeagueSet]);
   return (
     <div
       className="landing p-0"
@@ -34,36 +36,40 @@ const Landing = () => {
     >
       <TopNav></TopNav>
       <Row className="m-0">
-        <Col xs={0} sm={3} md={2} lg={2} xl={1} className="p-0">
-          <SideNav></SideNav>
-        </Col>
         <Col>
-          <Row>
-            <Col className="mt-5">
-              {!authState.isLeagueSet ? (
-                <h1>Select a League: {authState.user}</h1>
-              ) : (
-                `Selected League ${authState.user}`
-              )}
-              <Results></Results>
-            </Col>
-            <Col sm={2} className="mt-5 pt-2">
-              <Dropdown className="d-flex justify-content-end mr-4">
-                <Dropdown.Toggle
-                  variant="dark"
-                  id="dropdown-basic"
-                  drop={"down"}
-                >
-                  Tournaments
-                </Dropdown.Toggle>
+          <div style={{ height: "5vh" }}></div>
 
-                <Dropdown.Menu>
-                  {tournaments.map((val) => {
-                    console.log(val);
-                    return <Dropdown.Item>{val.name}</Dropdown.Item>;
-                  })}
-                </Dropdown.Menu>
-              </Dropdown>
+          {!isLeagueSet ? <h1>Select a League:</h1> : <h1>{selectedTour}</h1>}
+
+          <Dropdown className="d-flex justify-content-end mr-4">
+            <Dropdown.Toggle variant="dark" id="dropdown-basic" drop={"down"}>
+              Tournaments
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              {tournaments.map((val) => {
+                return (
+                  <Dropdown.Item
+                    onClick={() => {
+                      setSelectedTour(val.name);
+                      setIsLeagueSet(true);
+                      console.log(isLeagueSet);
+                      console.log(selectedTour);
+                    }}
+                  >
+                    {val.name}
+                  </Dropdown.Item>
+                );
+              })}
+            </Dropdown.Menu>
+          </Dropdown>
+
+          <Row>
+            <Col lg={2} className="mt-5 d-none d-md-block">
+              <SideNav></SideNav>
+            </Col>
+            <Col sm={12} md={12} lg={10} className="mt-5">
+              <Results></Results>
             </Col>
           </Row>
         </Col>
@@ -73,3 +79,52 @@ const Landing = () => {
 };
 
 export default Landing;
+
+/*
+ <Row>
+            <Col sm={2} className="mt-5">
+              <h1 style={{ color: "#131313" }}>,</h1>
+              <SideNav></SideNav>
+            </Col>
+            <Col className="mt-5">
+              <Row>
+                <Col>
+                  {!isLeagueSet ? (
+                    <h1>Select a League:</h1>
+                  ) : (
+                    <h1>{selectedTour}</h1>
+                  )}
+                </Col>
+                <Col>
+                  <Dropdown
+                    className="d-flex justify-content-end mr-4 mt-1"
+                    style={{ display: "inline-block" }}
+                  >
+                    <Dropdown.Toggle
+                      variant="dark"
+                      id="dropdown-basic"
+                      drop={"down"}
+                    >
+                      Tournaments
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                      {tournaments.map((val) => {
+                        return (
+                          <Dropdown.Item
+                            onClick={() => {
+                              setSelectedTour(val.name);
+                              setIsLeagueSet(true);
+                              console.log(isLeagueSet);
+                              console.log(selectedTour);
+                            }}
+                          >
+                            {val.name}
+                          </Dropdown.Item>
+                        );
+                      })}
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </Col>
+              </Row>
+              */
--- footclient/src/components/SideNav.js
+++ footclient/src/components/SideNav.js
@@ -6,19 +6,19 @@ const TopNav = () => {
   const { dispatch } = useContext(AuthContext);
   return (
     <Navbar
-      collapseOnSelect
       expand="sm"
-      style={{ backgroundColor: "#2b2b2b", height: "100vh" }}
+      style={{
+        backgroundColor: "#2b2b2b",
+        height: "80vh",
+        borderRadius: "16px 16px 16px 16px",
+      }}
     >
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav defaultActiveKey="/home" className="flex-column">
-          <Nav.Link eventKey="link-1">Next Guess</Nav.Link>
-          <Nav.Link eventKey="link-2">Results</Nav.Link>
-          <Nav.Link eventKey="link-3">Rules</Nav.Link>
-          <Nav.Link eventKey="link-4">All Time Rank</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
+      <Nav defaultActiveKey="/home" className="flex-column">
+        <Nav.Link eventKey="link-1">Next Guess</Nav.Link>
+        <Nav.Link eventKey="link-2">Results</Nav.Link>
+        <Nav.Link eventKey="link-3">Rules</Nav.Link>
+        <Nav.Link eventKey="link-4">All Time Rank</Nav.Link>
+      </Nav>
     </Navbar>
   );
 };
