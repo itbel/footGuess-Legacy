d24a51a9fc5b584b0a2d7a2186c1225932065c5d
Igor Belem <igortbelem@gmail.com>
2020-07-08 22:51:59 -0400

    some styling changes to the side nav

--- footclient/src/components/views/SideNav.js
+++ footclient/src/components/views/SideNav.js
@@ -1,24 +1,19 @@
 import React, { useContext } from "react";
 import { Navbar, Nav, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { Context } from "../Store";
 
 const SideNav = () => {
   return (
-    <Row className="m-0" xs={0}>
-      <Navbar expand="xs" bg="dark" variant="dark">
-        <Nav className="mr-auto">
-          <Link className="linkStyle" to="/guess">
-            Guess
-          </Link>
-          <Link className="linkStyle" to="/rules">
-            Rules
-          </Link>
-          <Link className="linkStyle" to="/ranking">
-            Tournament Ranking
-          </Link>
-        </Nav>
-      </Navbar>
+    <Row className="m-0 sideNav">
+      <Link className="side" to="/guess">
+        Guess
+      </Link>
+      <Link className="side" to="/rules">
+        Rules
+      </Link>
+      <Link className="side" to="/ranking">
+        Tournament Ranking
+      </Link>
     </Row>
   );
 };
