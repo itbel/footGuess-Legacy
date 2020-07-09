c231f3fd4038f4ad56654e5addac630d22af8ba0
Igor Belem <igortbelem@gmail.com>
2020-07-08 22:40:57 -0400

    table design changes

--- footclient/src/components/views/PointsTable.js
+++ footclient/src/components/views/PointsTable.js
@@ -27,58 +27,76 @@ const PointsTable = () => {
     }
   }, [state]);
   return (
-    <Container>
-      <Table striped hover variant="light">
-        <thead>
+    <table className="rankingTable">
+      <thead>
+        <tr>
+          <th>Match</th>
+          <th>Result</th>
+          {matches !== undefined && players !== undefined
+            ? players.map((entry1, key) => {
+                return <th key={key}>{entry1}</th>;
+              })
+            : null}
+        </tr>
+      </thead>
+      <tbody>
+        {matches !== undefined ? (
+          matches.map((match, key) => {
+            return (
+              <tr key={key}>
+                <td>
+                  {match.teamAName + " "}X{" " + match.teamBName}
+                </td>
+                <td>
+                  {match.teamAResult !== undefined ? (
+                    <b>{match.teamAResult}</b>
+                  ) : null}
+                  X
+                  {match.teamBResult !== undefined ? (
+                    <b>{match.teamBResult}</b>
+                  ) : null}
+                </td>
+                {players.map((player, key2) => {
+                  for (let i = 0; i < players.length; i++) {
+                    let found = match.guesses.find(
+                      (el) => el.player.name === player
+                    );
+                    return (
+                      <td>
+                        {found !== undefined
+                          ? `${found.teamAguess} X ${found.teamBguess} ${
+                              found.points !== 0 ? `+(${found.points})` : "(0)"
+                            }`
+                          : ""}
+                      </td>
+                    );
+                  }
+                })}
+              </tr>
+            );
+          })
+        ) : (
           <tr>
-            <th>Match</th>
-            {matches !== undefined && players !== undefined
-              ? players.map((entry1, key) => {
-                  return <th key={key}>{entry1}</th>;
-                })
-              : null}
+            <td>Nothing</td>
           </tr>
-        </thead>
-        <tbody>
-          {matches !== undefined ? (
-            matches.map((match, key) => {
-              return (
-                <tr key={key}>
-                  <td>
-                    {match.teamAName}
-                    {match.teamAResult !== undefined ? match.teamAResult : null}
-                    X
-                    {match.teamBResult !== undefined ? match.teamBResult : null}
-                    {match.teamBName}
-                  </td>
-                  {players.map((player, key2) => {
-                    for (let i = 0; i < players.length; i++) {
-                      let found = match.guesses.find(
-                        (el) => el.player.name === player
-                      );
-                      return (
-                        <td>
-                          {found !== undefined
-                            ? `${found.teamAguess} X ${found.teamBguess} +(${found.points})`
-                            : ""}
-                        </td>
-                      );
-                    }
-                  })}
-                </tr>
-              );
-            })
-          ) : (
-            <tr>
-              <td>Nothing</td>
-            </tr>
-          )}
-          <tr>
-            <td className="justify-content-center d-flex">Points</td>
-          </tr>
-        </tbody>
-      </Table>
-    </Container>
+        )}
+        <tr className="font-weight-bold">
+          <td className="justify-content-center d-flex">Points</td>
+          <td></td>
+          {players.map((player, key2) => {
+            let playerPoints = 0;
+            for (let i = 0; i < matches.length; i++) {
+              for (let x = 0; x < matches[i].guesses.length; x++) {
+                if (matches[i].guesses[x].player.name === player) {
+                  playerPoints += matches[i].guesses[x].points;
+                }
+              }
+            }
+            return <td>{`+${playerPoints}`}</td>;
+          })}
+        </tr>
+      </tbody>
+    </table>
   );
 };
 
